fdb905a22566e8d1679dedc7054143899fdc1899
Yang JianXin <995462226@qq.com>
2023-10-27 22:57:54 +0800

    update evaluate.py

--- script/evaluate/evaluate.py
+++ script/evaluate/evaluate.py
@@ -3,30 +3,31 @@ import math
 from loguru import logger
 import torch
 import argparse
-from component.dataset import EvalDataset
-from component.collator import EvalCollator
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 import sys
 sys.path.append("../../")
 from component.utils import ModelUtils
+from component.dataset import EvalDataset
+from component.collator import EvalCollator
 """
-单轮对话，不具有对话历史的记忆功能
+评测模型ppl
 """
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='arg parser')
     parser.add_argument('--model_name_or_path', type=str, default="NousResearch/Llama-2-7b-hf")
-    parser.add_argument('--adapter_name_or_path', type=str, default='')
+    parser.add_argument('--adapter_name_or_path', type=str, default='path-to-lora')
     # parser.add_argument('--eval_file', type=str, default="../../eval/data/proof-pile-test.bin", help='')
     # parser.add_argument('--eval_file', type=str, default="../../eval/data/pg19-test.bin", help='')
-    parser.add_argument('--eval_file', type=str, default="../../eval/data/pg19-validation.bin", help='')
-    parser.add_argument('--load_in_4bit', type=bool, default=False, help='')
-    parser.add_argument('--batch_size', type=int, default=32, help='batch size during inference')
-    parser.add_argument('--max_seq_length', type=int, default=8092, help='context length during evaluation')
-    parser.add_argument('--model_max_length', type=int, default=8092, help='context size during fine-tuning')
-    parser.add_argument('--sliding_window', type=int, default=256, help='context size during fine-tuning')
+    parser.add_argument('--eval_file', type=str, default="../../data/eval/pg19-validation.bin", help='')
+    parser.add_argument('--load_in_4bit', type=bool, default=True, help='')
+    parser.add_argument('--batch_size', type=int, default=1, help='batch size during inference')
+    parser.add_argument('--max_seq_length', type=int, default=7900, help='context length during evaluation')
+    parser.add_argument('--model_max_length', type=int, default=8192, help='context size during fine-tuning')
+    parser.add_argument('--sliding_window', type=int, default=7900, help='context size during fine-tuning')
     args = parser.parse_args()
     return args
 
@@ -61,6 +62,7 @@ def main():
         load_in_4bit=args.load_in_4bit,
         adapter_name_or_path=args.adapter_name_or_path
     ).eval()
+    print(f'memory footprint of model: {model.get_memory_footprint() / (1024 * 1024 * 1024)} GB')
     tokenizer = AutoTokenizer.from_pretrained(
         args.model_name_or_path,
         trust_remote_code=True,
@@ -68,16 +70,16 @@ def main():
         use_fast=False if model.config.model_type == 'llama' else True
     )
     # 加载评测数据
-    dataset = EvalDataset(args.eval_file, tokenizer, args.max_seq_length, args.sliding_window)
+    dataset = EvalDataset(args.eval_file, tokenizer, args.max_seq_length, -100, args.sliding_window)
     collator = EvalCollator(tokenizer, args.max_seq_length, ignore_index=-100)
     dataloader = DataLoader(dataset, collate_fn=collator, batch_size=args.batch_size)
 
     right_token_cnt = 0
     total_token_cnt = 0
     eval_loss = []
-    for batch in dataloader:
-        input_ids = batch['input_ids']
-        labels = batch['labels']
+    for batch in tqdm(dataloader):
+        input_ids = batch['input_ids'].to(model.device)
+        labels = batch['labels'].to(model.device)
         with torch.no_grad():
             out = model(input_ids, labels=labels)
 
@@ -90,6 +92,7 @@ def main():
     eval_loss = sum(eval_loss) / len(eval_loss)
     logger.info(f'Acc: {acc}')
     logger.info(f'Eval loss: {eval_loss}')
+    logger.info(f'Eval ppl: {2.71828**eval_loss}')
 
 
 if __name__ == '__main__':
