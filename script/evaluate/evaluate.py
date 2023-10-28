fd6385539a358dbef99d859c29f7b137d111037c
Yang JianXin <995462226@qq.com>
2023-10-28 22:20:54 +0800

    update evaluate.py

--- script/evaluate/evaluate.py
+++ script/evaluate/evaluate.py
@@ -1,99 +1,169 @@
-from transformers import AutoTokenizer, AutoConfig
+# Written by Yukang Chen
+# Some code based on https://github.com/epfml/landmark-attention
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import math
-from loguru import logger
 import torch
 import argparse
-from torch.utils.data import DataLoader
+import random
+import numpy as np
 from tqdm import tqdm
+import transformers
 
 import sys
 sys.path.append("../../")
 from component.utils import ModelUtils
-from component.dataset import EvalDataset
-from component.collator import EvalCollator
+from attention.llama_attn_replace import replace_llama_attn
 """
-评测模型ppl
+评测base model的ppl
 """
 
 
-def parse_args():
+def parse_config():
     parser = argparse.ArgumentParser(description='arg parser')
-    parser.add_argument('--model_name_or_path', type=str, default="NousResearch/Llama-2-7b-hf")
-    parser.add_argument('--adapter_name_or_path', type=str, default='path-to-lora')
-    # parser.add_argument('--eval_file', type=str, default="../../eval/data/proof-pile-test.bin", help='')
-    # parser.add_argument('--eval_file', type=str, default="../../eval/data/pg19-test.bin", help='')
-    parser.add_argument('--eval_file', type=str, default="../../data/eval/pg19-validation.bin", help='')
-    parser.add_argument('--load_in_4bit', type=bool, default=True, help='')
     parser.add_argument('--batch_size', type=int, default=1, help='batch size during inference')
-    parser.add_argument('--max_seq_length', type=int, default=7900, help='context length during evaluation')
-    parser.add_argument('--model_max_length', type=int, default=8192, help='context size during fine-tuning')
-    parser.add_argument('--sliding_window', type=int, default=7900, help='context size during fine-tuning')
+    parser.add_argument('--base_model', type=str, default="NousResearch/Llama-2-7b-hf")
+    parser.add_argument('--seq_len', type=int, default=8192, help='context length during evaluation')
+    parser.add_argument('--context_size', type=int, default=8192, help='context size during fine-tuning')
+    parser.add_argument('--sliding_window', type=int, default=8192, help='context size during fine-tuning')
+    parser.add_argument('--peft_model', type=str, default=None, help='')
+    parser.add_argument('--flash_attn', type=bool, default=False, help='')
+    parser.add_argument('--load_in_4bit', type=bool, default=True, help='weather use 4 bit to inference')
+    # parser.add_argument('--data_path', type=str, default="../../data/proof-pile-test.bin", help='')
+    # parser.add_argument('--data_path', type=str, default="../../data/pg19-test.bin", help='')
+    parser.add_argument('--data_path', type=str, default="../../data/eval/pg19-validation.bin", help='')
     args = parser.parse_args()
     return args
 
 
-def calculate_acc(logit, labels, ignore_index=-100):
-    logit = logit[..., :-1, :].contiguous().view(-1, logit.size(-1))
-    labels = labels[..., 1:].contiguous().view(-1)
+def get_as_batch(data, seq_length, batch_size, device='cpu', sliding_window=256):
+    all_ix = list(range(0, len(data) - seq_length, sliding_window))
+    all_ix.pop()
 
-    _, logit = logit.max(dim=-1)  # 对于每条数据，返回最大的index
-    # 进行非运算，返回一个tensor，若labels的第i个位置为pad_id，则置为0，否则为1
-    non_pad_mask = labels.ne(ignore_index)
-    n_correct = logit.eq(labels).masked_select(non_pad_mask).sum().item()
-    n_word = non_pad_mask.sum().item()
-    acc = n_correct/n_word
-    return n_correct, n_word, acc
+    for idx in range(0, len(all_ix), batch_size):
+        ix = all_ix[idx:idx + batch_size]
+        assert all([idx + seq_length + 1 <= len(data) for idx in ix])
+        x = torch.stack([torch.from_numpy((data[i:i + seq_length]).astype(np.int64)) for i in ix])
+        y = torch.stack([torch.from_numpy((data[i + 1:i + 1 + seq_length]).astype(np.int64)) for i in ix])
+        if device != 'cpu':
+            x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+        yield x, y
 
 
-def main():
-    args = parse_args()
-    # 修改RoPE的position最大长度
-    config = AutoConfig.from_pretrained(args.model_name_or_path)
-    orig_ctx_len = getattr(config, "max_position_embeddings", None)
-    if orig_ctx_len and args.model_max_length > orig_ctx_len:
-        scaling_factor = float(math.ceil(args.model_max_length / orig_ctx_len))
+def iceildiv(x, y):
+    return (x + y - 1) // y
+
+
+def evaluate(model, data, batch_size, device, seq_length, sliding_window=256, use_cache=False):
+    stats = {}
+
+    model.eval()
+
+    loss_list_val, acc_list = [], []
+    loss_step_list_val = []
+
+    with torch.no_grad():
+        print(f"Using seq length {seq_length}")
+        torch.set_printoptions(sci_mode=False)
+        for idx, (x, y) in tqdm(
+                enumerate(
+                    get_as_batch(
+                        data['val'],
+                        seq_length,
+                        batch_size,
+                        device=device,
+                        sliding_window=sliding_window
+                    )
+                ),
+                total=iceildiv(
+                    iceildiv(len(data['val']), sliding_window),
+                    batch_size
+                )
+        ):
+            val_loss = 0.
+            acc = 0.
+            cnt = 0
+
+            for part_idx, i in enumerate(range(0, x.shape[1], seq_length)):
+                part_len = x[:, i:i + seq_length].shape[1]
+
+                outputs = model(
+                    input_ids=x[:, i:i + seq_length],
+                    labels=x[:, i:i + seq_length].contiguous(),
+                    use_cache=use_cache)
+
+                val_loss = outputs.loss * part_len + val_loss
+                acc = ((outputs.logits.argmax(-1) == y[:, i:i + seq_length]).float().sum()) + acc
+                cnt += part_len
+                while len(loss_step_list_val) <= part_idx:
+                    loss_step_list_val.append([])
+                loss_step_list_val[part_idx].append(outputs.loss.item())
+            val_loss /= cnt
+            acc /= cnt
+
+            loss_list_val.append(val_loss.item())
+            acc_list.append(acc.item())
+
+    stats['val_acc'] = torch.as_tensor(acc_list).mean().item()
+    stats['val_loss'] = torch.as_tensor(loss_list_val).mean().item()
+    stats['val_perplexity'] = 2.71828 ** stats['val_loss']
+    stats['val_perplexity_per_chunk'] = torch.exp(torch.as_tensor(loss_step_list_val).mean(dim=1))
+
+    return stats
+
+
+def main(args):
+    device = "cuda:0"
+    seed = 2
+    torch.cuda.set_device(device)
+
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    data = {'val': np.memmap(args.data_path, dtype=np.uint16, mode='r')}
+
+    print(f"Num validation tokens: {len(data['val'])}")
+    print("data path", args.data_path)
+    print("base model", args.base_model)
+    print("peft model", args.peft_model)
+
+    if args.flash_attn:
+        replace_llama_attn(use_flash_attn=True, use_full=True)
+
+    # Set RoPE scaling factor
+    config = transformers.AutoConfig.from_pretrained(args.base_model)
+    context_size = args.context_size if args.context_size > 0 else args.seq_len
+    orig_ctx_len = getattr(config, "max_position_embeddings", None)  # this value should be 4096 for LLaMA2 models
+    if orig_ctx_len and context_size > orig_ctx_len:
+        scaling_factor = float(math.ceil(context_size / orig_ctx_len))
         config.rope_scaling = {"type": "linear", "factor": scaling_factor}
-    logger.info(f'Change model_max_length from {orig_ctx_len} to {args.model_max_length}')
 
-    # 加载模型
+    # Load model and tokenizer
     model = ModelUtils.load_model(
-        args.model_name_or_path,
+        args.base_model,
         config=config,
         load_in_4bit=args.load_in_4bit,
-        adapter_name_or_path=args.adapter_name_or_path
+        adapter_name_or_path=args.peft_model
     ).eval()
-    print(f'memory footprint of model: {model.get_memory_footprint() / (1024 * 1024 * 1024)} GB')
-    tokenizer = AutoTokenizer.from_pretrained(
-        args.model_name_or_path,
-        trust_remote_code=True,
-        # llama不支持fast
-        use_fast=False if model.config.model_type == 'llama' else True
-    )
-    # 加载评测数据
-    dataset = EvalDataset(args.eval_file, tokenizer, args.max_seq_length, -100, args.sliding_window)
-    collator = EvalCollator(tokenizer, args.max_seq_length, ignore_index=-100)
-    dataloader = DataLoader(dataset, collate_fn=collator, batch_size=args.batch_size)
+    # model.resize_token_embeddings(32001)
 
-    right_token_cnt = 0
-    total_token_cnt = 0
-    eval_loss = []
-    for batch in tqdm(dataloader):
-        input_ids = batch['input_ids'].to(model.device)
-        labels = batch['labels'].to(model.device)
-        with torch.no_grad():
-            out = model(input_ids, labels=labels)
+    stats = evaluate(model, data, args.batch_size, device, args.seq_len, sliding_window=args.sliding_window)
 
-        eval_loss.append(out.loss.item())
-        n_correct, n_word, _ = calculate_acc(out.logits, labels, ignore_index=-100)
-        right_token_cnt += n_correct
-        total_token_cnt += n_word
-
-    acc = right_token_cnt/total_token_cnt
-    eval_loss = sum(eval_loss) / len(eval_loss)
-    logger.info(f'Acc: {acc}')
-    logger.info(f'Eval loss: {eval_loss}')
-    logger.info(f'Eval ppl: {2.71828**eval_loss}')
+    print(stats)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    args = parse_config()
+    main(args)
